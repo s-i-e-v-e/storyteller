b75ccdc6b9bb8a85342f34f16e7760010b81bb27
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2025-02-04 17:07:31 +0000

    include files

--- src/storyteller/app.py
+++ src/storyteller/app.py
@@ -4,24 +4,47 @@ def fix_demented_path_resolution(mod: str):
         if sys.path[i].endswith(f'/src/{mod}'):
             sys.path[i] = '/'.join(sys.path[i].split('/')[:-1])
 
-def replace(model: str, file: str):
-    from storyteller import config
-    from nonstd import fs
+from dataclasses import dataclass
+@dataclass
+class State:
+    xs: str
+    index: int
 
-    c = config.load()
-    m = c.models[model]
+def handle_include(f: str):
+    pass
 
-
-    data = fs.read_text(file)
+def find_last_context(st: State):
+    data = st.xs
     ix = data.rfind('\\context')
     iy = data.find('\\end', ix)
     if ix < 0 or iy < 0:
         raise Exception
     ix += len('\\context')
+    return data[ix:iy]
 
-    from storyteller import llm
-    content = llm.query(m, data[ix:iy])
+def load_context(st: State):
+    from nonstd import fs
+    st = State(find_last_context(st), 0)
+    while True:
+        ix = st.xs.find('\\include ')
+        if ix == -1:
+            break
+        iy = st.xs.find('\n', ix)
+        ixx = ix + len('\\include ')
+        fn = st.xs[ixx:iy]
+        x = fs.read_text(fn)
+        st.xs = st.xs[:ix] + x + st.xs[iy:]
+    return st.xs
 
+def replace(model: str, file: str):
+    from nonstd import fs
+    from storyteller import config, llm
+
+    c = config.load()
+    m = c.models[model]
+    st = State(fs.read_text(file), 0)
+    data = load_context(st)
+    content = llm.query(m, data)
     fs.append_text(file, content)
 
 def main():
